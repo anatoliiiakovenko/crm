a027e361a1d7d8064a8a41e44127e8efe1cf5a8a
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2024-05-03 15:00:01 +0300

    add labels

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import {Status, StatusLabel} from "@/app/components/status-label";
 
 export default function Home() {
   return (
     <main>
-      <h1>Home page</h1>
+        <h1>Home page</h1>
+        <StatusLabel status={Status.Active}>Active</StatusLabel>
+        <StatusLabel status={Status.Inactive}>Inactive</StatusLabel>
+        <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+        <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
     </main>
   );
 }
